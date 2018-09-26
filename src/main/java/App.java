cf59fd70a18c1c15ea957e3b2a3a0856644d917b
augustynSybidlo <augustyn.sybidlo@gmail.com>
2018-09-26 13:22:11 +0200

    Add setup to test main class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,8 @@
 import controller.TransformController;
+import dao.PersonDAO;
+import dao.PersonFromCVS;
 import model.Person;
+import service.CSValidator;
 import service.Transform;
 import view.PersonView;
 
@@ -16,12 +19,24 @@ public class App {
         input.add(new Person("Zosia", 9));
         input.add(new Person("Wiola", 7));
         input.add(new Person("Arek", 20));
-
         return input;
     }
 
+    private static List<Person> setup(String path, int numberOfColumns) {
+        PersonDAO dao = new PersonFromCVS(path, new CSValidator(), numberOfColumns);
+        return dao.read();
+    }
+
     public static void main(String[] args) {
-        Transform transform = new Transform(App.setup());
+//        Transform transform = new Transform(setup());
+//        TransformController controller = new TransformController(transform, new PersonView());
+//        controller.run();
+//        Transform transform = new Transform(setup(args[0], 2));
+//        TransformController controller = new TransformController(transform, new PersonView());
+//        controller.run();
+
+        Transform transform = new Transform(setup("/Users/augustyn/Documents/GitHub/" +
+                "assignment-augustynSybidlo/src/main/resources/test.csv", 2));
         TransformController controller = new TransformController(transform, new PersonView());
         controller.run();
     }
